1882a0da7111547ed9ab991f0219ae4887cfcb15
Ushio Okasaki <haloleyendee@outlook.com>
2022-01-06 18:50:16 -0600

    feat(java): Added functionality for the detection of the operating system and its architecture

---
+++ jvm_utilities/src/main/java/com/github/ushiosan23/jvm/system/os/Platform.java
@@ -0,0 +1,105 @@
+package com.github.ushiosan23.jvm.system.os;
+
+import com.github.ushiosan23.jvm.base.StringUtils;
+import org.intellij.lang.annotations.RegExp;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public enum Platform {
+	LINUX("^(linux)"),
+	MACOS("^(mac os|mac)"),
+	SOLARIS("^(sunos|sun)"),
+	WINDOWS("^(windows|win)"),
+	FREE_BSD("^(freebsd|free)"),
+	UNKNOWN;
+
+	/* ------------------------------------------------------------------
+	 * Properties
+	 * ------------------------------------------------------------------ */
+
+	/**
+	 * Regular expression to verify platform
+	 */
+	@Nullable
+	private final Pattern pattern;
+
+	/* ------------------------------------------------------------------
+	 * Constructors
+	 * ------------------------------------------------------------------ */
+
+	/**
+	 * Default constructor
+	 *
+	 * @param regex Regular expression to verify platform
+	 */
+	Platform(@Nullable @RegExp String regex) {
+		pattern = regex == null ? null : Pattern.compile(regex);
+	}
+
+	/**
+	 * Null constructor
+	 */
+	Platform() {
+		this(null);
+	}
+
+	/* ------------------------------------------------------------------
+	 * Methods
+	 * ------------------------------------------------------------------ */
+
+	/**
+	 * Determine if the platform is unix-like
+	 *
+	 * @return Returns {@code true} if platform is unix-like or {@code false} otherwise
+	 */
+	public boolean isUnix() {
+		switch (this) {
+			case LINUX:
+			case MACOS:
+			case SOLARIS:
+			case FREE_BSD:
+				return true;
+			default:
+				return false;
+		}
+	}
+
+	/**
+	 * Returns the current platform where the JVM is running
+	 *
+	 * @return Returns the platform where the JVM runs or {@link #UNKNOWN} if the platform is not listed
+	 */
+	public static Platform getCurrent() {
+		// Get all platforms
+		Platform[] platforms = values();
+		String osName = System.getProperty("os.name").toLowerCase(Locale.ROOT);
+		// Iterate all
+		for (Platform platform : platforms) {
+			// Ignore null patterns
+			if (platform.pattern == null) continue;
+			// Match result
+			Matcher matcher = platform.pattern.matcher(osName);
+			if (matcher.find()) return platform;
+		}
+		return UNKNOWN;
+	}
+
+	/**
+	 * Object string representation
+	 *
+	 * @return Object string representation
+	 */
+	@Override
+	public @NotNull String toString() {
+		return StringUtils.capitalize(
+			super.toString()
+				.replace("_", " ")
+				.toLowerCase(Locale.ROOT)
+		);
+	}
+
+}

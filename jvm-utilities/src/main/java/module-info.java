3abc302c271ee30e70a3964e3b4ab2533afa8ce6
Ushio Okasaki <haloleyendee@outlook.com>
2022-09-27 22:42:31 -0500

    refactor(jvm-utilities): The module is now more intuitive and has a name according to the standards and the packages are shorter

---
+++ jvm-utilities/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.github.ushiosan.jvm_utilities {
+	requires static org.jetbrains.annotations;
+
+	exports ushiosan.jvm_utilities.error;
+	exports ushiosan.jvm_utilities.function;
+	exports ushiosan.jvm_utilities.function.predicate;
+	exports ushiosan.jvm_utilities.lang;
+	exports ushiosan.jvm_utilities.lang.collection;
+	exports ushiosan.jvm_utilities.lang.collection.elements;
+	exports ushiosan.jvm_utilities.lang.io;
+	exports ushiosan.jvm_utilities.lang.print;
+	exports ushiosan.jvm_utilities.lang.print.annotations;
+	exports ushiosan.jvm_utilities.system;
+}

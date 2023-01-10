72205f0e7d4d66bf4b17cd7dccbc150c73ef2809
Ushiosan23 <haloleyendee@outlook.com>
2023-01-10 16:15:04 -0600

    refactor(jvm-utilities): jvm-utilities 0.4.0

--- buildSrc/src/main/kotlin/defined/project/JvmUtilitiesProject.kt
+++ buildSrc/src/main/kotlin/defined/project/JvmUtilitiesProject.kt
@@ -14,7 +14,7 @@ import publishing.*
 val JvmUtilitiesProject.releasePublication: PublicationInfo
 	get() = PublicationInfo(
 		name = "release",
-		version = "0.3.5",
+		version = "0.4.0",
 		pom = PublicationPom(
 			artifactUrl = "https://github.com/Ushiosan23/jvm_libs.git",
 			description = "Utilities for the java virtual machine.",

ef3a309a291f83adffb36ab67d88db003664abd7
Ushiosan23 <haloleyendee@outlook.com>
2023-01-11 12:19:40 -0600

    refactor(jvm-utilities): Now it is possible to know what type each of the elements of the array is, even if it is empty.

--- jvm-utilities/src/main/java/ushiosan/jvm_utilities/lang/Cls.java
+++ jvm-utilities/src/main/java/ushiosan/jvm_utilities/lang/Cls.java
@@ -3,6 +3,11 @@ package ushiosan.jvm_utilities.lang;
 import org.jetbrains.annotations.NotNull;
 import ushiosan.jvm_utilities.lang.collection.Arrs;
 
+import static ushiosan.jvm_utilities.lang.Obj.cast;
+
+/**
+ * Class containing helper methods for working with objects of type {@link Class}
+ */
 public final class Cls {
 	
 	/**
@@ -23,6 +28,7 @@ public final class Cls {
 	/* -----------------------------------------------------
 	 * Methods
 	 * ----------------------------------------------------- */
+	
 	/**
 	 * Java primitive array types
 	 */
@@ -35,6 +41,20 @@ public final class Cls {
 		long[].class,
 		float[].class,
 		double[].class
+	};
+	
+	/**
+	 * Reference to the wrapper class for primitive types in arrays.
+	 */
+	private static final Class<?>[] PRIMITIVE_ARRAY_INDIVIDUAL = new Class[]{
+		boolean.class,
+		char.class,
+		byte.class,
+		short.class,
+		int.class,
+		long.class,
+		float.class,
+		double.class
 	};
 	
 	/**
@@ -103,6 +123,99 @@ public final class Cls {
 		return java.util.Arrays.stream(args)
 			.map(Object::getClass)
 			.toArray(Class[]::new);
+	}
+	
+	/**
+	 * Returns the class of the individual object in an array.
+	 * <p>
+	 * <b>Example:</b>
+	 * <pre>{@code
+	 * String[] array = new String[] {"Hello", "World", "!"};
+	 * Class<?> cls = Cls.getArrayIndividualClass(array);
+	 *
+	 * // Output -> class java.lang.String
+	 * }</pre>
+	 *
+	 * @param array the array you want to identify
+	 * @param <T>   the class of the data type
+	 * @return the data type of the elements individually
+	 */
+	public static <T> @NotNull Class<T> getArrayIndividualClass(T @NotNull [] array) {
+		Class<?> generalClass = array.getClass();
+		return getArrayIndividualClassImpl(generalClass);
+	}
+	
+	/**
+	 * Returns the class of the individual object in an array.
+	 * <p>
+	 * <b>Example:</b>
+	 * <pre>{@code
+	 * String[][] array = new String[0][0];
+	 * Class<?> cls = Cls.getArrayIndividualClass(array);
+	 *
+	 * // Output -> class java.lang.String
+	 * }</pre>
+	 *
+	 * @param array the array you want to identify
+	 * @param <T>   the class of the data type
+	 * @return the data type of the elements individually
+	 */
+	public static <T> @NotNull Class<T> getArrayIndividualClass(T @NotNull [] @NotNull [] array) {
+		Class<?> generalClass = array.getClass();
+		return getArrayIndividualClassImpl(generalClass);
+	}
+	
+	/**
+	 * Returns the class of the individual object in an array.
+	 * <p>
+	 * <b>Example:</b>
+	 * <pre>{@code
+	 * String[][][] array = new String[0][0][0]; // or more
+	 * Class<?> cls = Cls.getArrayIndividualClass(array);
+	 *
+	 * // Output -> class java.lang.String
+	 * }</pre>
+	 *
+	 * @param array the array you want to identify
+	 * @param <T>   the class of the data type
+	 * @return the data type of the elements individually
+	 */
+	public static <T> @NotNull Class<T> getArrayMultipleIndividualClass(@NotNull Object array) {
+		Class<?> cls = array.getClass();
+		// Check if is a valid array
+		if (!cls.isArray()) throw new IllegalArgumentException("Is not valid array object");
+		return getArrayIndividualClassImpl(cls);
+	}
+	
+	/* ---------------------------------------------------------
+	 * Internal methods
+	 * --------------------------------------------------------- */
+	
+	/**
+	 * Returns the individual name of a class
+	 *
+	 * @param cls the class you want to identify
+	 * @param <T> the class of the data type
+	 * @return the data type of the elements individually
+	 */
+	private static <T> @NotNull Class<T> getArrayIndividualClassImpl(@NotNull Class<?> cls) {
+		// Check if class is an array
+		if (!cls.isArray()) throw new IllegalArgumentException("Invalid array type");
+		String generalName = cls.getCanonicalName();
+		int index = Arrs.indexOf(PRIMITIVE_ARRAY_CLASSES, cls);
+		
+		// Only for primitive types
+		if (index != Arrs.INDEX_NOT_FOUND) {
+			return cast(PRIMITIVE_ARRAY_INDIVIDUAL[index]);
+		}
+		
+		// Clean class name
+		generalName = generalName.replaceAll("\\[]", "").trim();
+		try {
+			return cast(Class.forName(generalName));
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
 	
 }

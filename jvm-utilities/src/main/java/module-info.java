432817736c3fd7b90638377b9c61abcd94097cc1
Ushiosan23 <haloleyendee@outlook.com>
2023-05-23 13:09:47 -0600

    refactor(jvm-libraries): java module created

---
+++ jvm-utilities/src/main/java/module-info.java
@@ -0,0 +1,20 @@
+/**
+ *
+ */
+module ushiosan.jvm.utilities {
+	// Dependencies
+	requires java.xml;
+	requires java.logging;
+	requires static org.jetbrains.annotations;
+	
+	// Exports
+	exports ushiosan.jvm;
+	exports ushiosan.jvm.collections;
+	exports ushiosan.jvm.content;
+	exports ushiosan.jvm.error;
+	exports ushiosan.jvm.filesystem;
+	exports ushiosan.jvm.function;
+	exports ushiosan.jvm.platform;
+	exports ushiosan.jvm.print;
+	exports ushiosan.jvm.reflection;
+}

c4a3760fe3d410c2a32c48709007863487cac54b
Ushiosan23 <haloleyendee@outlook.com>
2023-07-21 19:36:38 -0600

    refactor(jvm-libraries): New version of "jvm-utilities"

--- buildSrc/src/main/kotlin/common/common-versions.kt
+++ buildSrc/src/main/kotlin/common/common-versions.kt
@@ -1,10 +1,22 @@
 package common
 
+/* -----------------------------------------------------
+ * Library versions
+ * ----------------------------------------------------- */
+
 val jvmUtilitiesVersion
-	get() = "1.0.0b2"
+	get() = "1.0.0b4"
 
 val jvmTestUtilitiesVersion
+	get() = "1.0.0b2"
+
+// Coming son
+val jvmLoggingVersion
 	get() = "1.0.0b1"
+
+/* -----------------------------------------------------
+ * Dependency versions
+ * ----------------------------------------------------- */
 
 val jetbrainsAnnotationsVersion
 	get() = "24.0.1"
